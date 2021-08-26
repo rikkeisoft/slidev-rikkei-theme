069cf7c03241a1109e8359ae292eafcf0fa0f39f
HuyNQ <huy@huynq.net>
2021-08-26 09:30:00 +0700

    Add two-cols & image-right layout

---
+++ themes/rikkei/layouts/image-right.vue
@@ -0,0 +1,36 @@
+<template>
+  <default class="intro" :logoHeader="logoHeader">
+    <div class="flex items-center">
+      <div class="w-1/2 px-12">
+        <slot />
+      </div>
+      <figure class="w-1/2">
+        <img
+          :src="introImage"
+          height="312"
+          width="312"
+          class="
+            rounded-3xl
+            p-10
+            bg-transparent
+            shadow-sm
+            rounded
+            filter
+          "
+        />
+      </figure>
+    </div>
+  </default>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineComponent } from "vue";
+import Default from "../layouts/default.vue";
+
+const components = defineComponent({ Default });
+const props = defineProps({
+  introImage: {
+    type: String,
+  },
+});
+</script>
---
+++ themes/rikkei/layouts/two-cols.vue
@@ -0,0 +1,38 @@
+<!--
+  Usage:
+```md
+---
+layout: two-cols
+---
+# Left
+This shows on the left
+::right::
+# Right
+This shows on the right
+```
+-->
+
+<script setup lang="ts">
+import { defineProps, defineComponent } from "vue";
+import Default from "../layouts/default.vue";
+
+const components = defineComponent({ Default });
+const props = defineProps({
+  class: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <default class="intro" :logoHeader="logoHeader">
+    <div class="slidev-layout two-columns w-full h-full grid grid-cols-2">
+      <div class="col-left" :class="props.class">
+        <slot />
+      </div>
+      <div class="col-right" :class="props.class">
+        <slot name="right" />
+      </div>
+    </div>
+  </default>
+</template>
